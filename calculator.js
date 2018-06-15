3934f498bda7d7c5da44cc6dda51445752a43ccd
Abby Zhang <abbyzhang@Abbys-MacBook-Pro.local>
2018-06-14 23:37:37 -1000

    calculator complete

--- calculator.js
+++ calculator.js
@@ -6,12 +6,107 @@
  * @return {object} `calculator` object that can be used
  */
 
+function calculatorModule(x, total){
+    var x = 0;
+    var total = 0;
+    //number passed in
+    var load = function(x){
+        if(typeof x !== 'number'){
+            throw err;
+        }else{
+            total = x;
+            return total;
+        }
+    };
+    //Return the value of `total`
+    var getTotal = function(){
+        return total;
+
+    };
+    //Sums the value passed in with `total`
+    var add = function(x){
+        if(typeof x !== 'number'){
+            throw err;
+        }else{
+            total += x;
+            return total;
+        }
+    };
+    //Subtracts the value passed in from `total`
+    var subtract = function(x){
+        if(typeof x !== 'number'){
+            throw err;
+        }else{
+            total -= x;
+            return total;
+        }
+    };
+    //Multiplies the value by `total`
+    var multiply = function(x){
+        if(typeof x !== 'number'){
+            throw err;
+        }else{
+            total *= x;
+            return total;
+        }
+    };
+    //Divides the value passing in by `total`
+    var divide = function(x){
+        if(typeof x !== 'number'){
+            throw err;
+        }else{
+            total /= x;
+            return total;
+        }
+    };
+    //Return the value stored at `memory`
+    var recallMemory = function(){
+        if(typeof x !== 'number'){
+            throw err;
+        }else{
+            return x;
+        }
+    };
+    //Stores the value of `total` to `memory`
+    var saveMemory = function(){
+        if(typeof x !== 'number'){
+            throw err;
+        }else{
+            x = total;
+            return x;
+        }
+    };
+    //Clear the value stored at `memory`
+    var clearMemory = function(){
+        if(typeof x !== 'number'){
+            throw err;
+        }else{
+            x = 0;
+            return x;
+        }
+        
+    };  
+    return {
+        load: load,
+        getTotal: getTotal,
+        add: add,
+        subtract: subtract,
+        multiply: multiply,
+        divide: divide,
+        recallMemory: recallMemory,
+        saveMemory: saveMemory,
+        clearMemory: clearMemory,
+
+    }
+}
 
   /**
    * sets the `total` to the number passed in
    * @param  { Number } x
    * @return { Number }    current total
    */
+ 
+
 
 
   /**
